0ef684e39cc09134343e531aa16ea70a58d31ac0
Gpx <polvara@gmail.com>
2019-07-03 10:14:19 +0200

    auto format commited files with prettier

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-border: none;
-background-color: #F9FAFC;
-color: #000;
-padding: 0.5em 1em;
-border-radius: 6px;
-font-weight: bold;
-font-size: 1.2rem;
-cursor: pointer;
-box-shadow: 0 0 4px 2px #0003;
+  border: none;
+  background-color: #f9fafc;
+  color: #000;
+  padding: 0.5em 1em;
+  border-radius: 6px;
+  font-weight: bold;
+  font-size: 1.2rem;
+  cursor: pointer;
+  box-shadow: 0 0 4px 2px #0003;
 `
 
 export default Button
