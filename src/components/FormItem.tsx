2e071335dcacab274f418be243f42f403a8dcac4
Gpx <polvara@gmail.com>
2019-07-16 12:07:04 +0200

    Style create form

--- src/components/FormItem.tsx
+++ src/components/FormItem.tsx
@@ -1,6 +1,18 @@
 import styled from 'styled-components/macro'
+import { color } from '../utils/variables'
 export const FormItem = styled.div`
   display: flex;
   flex-direction: column;
   margin-bottom: 2em;
+
+  label {
+    font-size: 1.3em;
+    margin-bottom: 0.3em;
+  }
+
+  input[type='text'],
+  textarea {
+    font-size: 1.1em;
+    border: 1px solid ${color.steel};
+  }
 `
